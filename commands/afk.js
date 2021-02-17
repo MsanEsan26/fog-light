559e5e99f75296a2b08e859a7553b55fac72f7d3
MsanEsan26 <75089450+MsanEsan26@users.noreply.github.com>
2021-02-17 09:57:01 +0300

    Add files via upload

---
+++ commands/afk.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+const data = require('quick.db');
+
+exports.run = async (client, message, args) => {
+
+if(message.author.id === message.guild.owner.user.id) return message.channel.send(new Discord.MessageEmbed().setTitle('**Seni gidi seni, Sunucu sahibisin sana __AFK__ yakışmaz ^^**'));
+if(!args[0]) reason = 'Bilgi verilmedi.';
+if(args[0]) reason = args[0];
+
+message.channel.send(new Discord.MessageEmbed().setThumbnail(message.author.avatarURL() ? message.author.avatarURL({dynamic: true}) : 'https://cdn.discordapp.com/attachments/810798391554342955/811481095161511986/botlogopng.png').setTitle(`${client.user.username} - Away From Keyboard`).setDescription(`${message.author} **Üyesi Bilgisayar başında & Klavye başında değil**!\n\n• **Sebep:** \`${reason}\``)).then(a => a.delete({timeout: 10000}));
+data.set(`name.${message.author.id}.${message.guild.id}`, message.member.displayName);
+message.member.setNickname('[AFK] '+message.member.displayName);
+data.set(`kullanıcı.${message.author.id}.${message.guild.id}`, reason);
+
+};
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: [],
+  permLevel: 0
+}
+
+exports.help = {
+  name: 'afk'
+};
